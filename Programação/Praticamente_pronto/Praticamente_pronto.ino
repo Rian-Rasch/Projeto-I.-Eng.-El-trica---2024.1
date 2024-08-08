64a59cafe18e56895ac23728508acb422a992847
Rian-Rasch <riankide@gmail.com>
2024-08-08 12:47:49 -0300

    programação

--- Programação/Praticamente_pronto/Praticamente_pronto.ino
+++ Programação/Praticamente_pronto/Praticamente_pronto.ino
@@ -1,14 +1,16 @@
+
 #include <Wire.h>  // Biblioteca para Comunicação I2C
 #include <LiquidCrystal_I2C.h>  // Biblioteca com as funções do display
 #include <Servo.h>
 
 #define flameSensorPin A0
 #define tempSensorPin A1
-#define batterySensorPin A2 // Supondo que o sensor de carga da bateria está conectado ao pino A2
+#define batterySensorPin 2 // Supondo que o sensor de carga da bateria está conectado ao pino A2
 #define servoPin 3
 
 #define p_temp 60
 #define p_fogo 200
+#define abertura 95
 Servo servo;
 
 LiquidCrystal_I2C lcd(0x27,20,4); // Inicializa o display no endereço 0x27
@@ -16,6 +18,8 @@ LiquidCrystal_I2C lcd(0x27,20,4); // Inicializa o display no endereço 0x27
 void setup() {
   Serial.begin(9600);
   servo.attach(servoPin);
+  pinMode(tempSensorPin, INPUT);
+  pinMode(flameSensorPin, INPUT);
   servo.write(0);
   lcd.init(); 
   lcd.begin(16, 2); // Identifica que o display possui 16 colunas e 2 linhas
@@ -29,16 +33,17 @@ int fogo_ir() {
     if (analogRead(flameSensorPin) < p_fogo) {
       return 1;
     } else return 0;
-    }
-  } else {
-    return 0;
-  }
+    }else return 0;
 }
 
 int fogo_temp() {
-  if (map(analogRead(tempSensorPin), 0, 1023, 125, -55) > p_temp) {
+  int v_input = analogRead(tempSensorPin);//1024 VALUE
+  float temp = 125.315-0.175529*v_input-2;
+  if (temp > p_temp) {
     delay(250);
-    if (map(analogRead(tempSensorPin), 0, 1023, 125, -55) > p_temp) {
+    int v_input = analogRead(tempSensorPin);//1024 VALUE
+    float temp = 125.315-0.175529*v_input-2;
+    if (temp > p_temp) {
       return 1;
     } else {
       return 0;
@@ -49,40 +54,60 @@ int fogo_temp() {
 }
 
 void aciona_ext(int flag){
-  int tempo = flag * 1000;
-  servo.write(360);
-  delay (tempo);
+  //int tempo = flag * 1000;
+  servo.write(abertura);
+  
+  while(flag !=0){
+    flag = fogo_temp() + fogo_ir();
+    
+  }
+  //delay (tempo);
   servo.write(0);
 }
 
 void loop() {
   int flag = fogo_temp() + fogo_ir();
+  int v_input = analogRead(tempSensorPin);//1024 VALUE
+  float temp = 125.315-0.175529*v_input-2;
+
 /*
   Serial.print("Flag = ");
   Serial.println(flag);
-  
   Serial.print("Temperatura: ");
-  Serial.print(map(analogRead(tempSensorPin), 0, 1023, 125, -55));
+  Serial.print(temp);
   Serial.println(" C ");
-  
+  Serial.print("IR: ");
+  Serial.println(analogRead(flameSensorPin));
+  /*
   Serial.print("Bat: ");
   Serial.print(map(analogRead(batterySensorPin), 0, 1023, 0, 100));
   Serial.println("%");
-  
   Serial.println();
-*/
-  delay(1000);
+  */
 
   lcd.clear(); // Apaga todos os caracteres do Display
   lcd.setCursor(0,0); // Marca o cursor na posição coluna 0, linha 0
-  lcd.print("Temperatura:");
-  lcd.print(map(analogRead(tempSensorPin), 0, 1023, 125, -55));
+  lcd.print("Temp:");
+  char buffer[10];
+  dtostrf(temp, 3, 0, buffer);
+  lcd.print(buffer);
   lcd.print("C");
-
   lcd.setCursor(0,1); // Marca o cursor na posição coluna 0, linha 1
   lcd.print("Bat:");
-  lcd.print(map(analogRead(batterySensorPin), 0, 1023, 0, 100));
+  dtostrf((2.54 + (analogRead(batterySensorPin) / 1023.0) * (4.25 - 2.54))*100/4.25, 3, 0, buffer);
+  lcd.print(buffer);
   lcd.print("%");
-  lcd.print(" Ir: ");
-  lcd.print(analogRead(flameSensorPin));
+  
+  //*(5.0/1023.0))
+  
+  lcd.setCursor(10,0);
+  lcd.print("Ir:");
+  lcd.print(analogRead(flameSensorPin)/100);
+
+   if(flag != 0){
+    aciona_ext(flag);
+  }
+
+  delay(500);
+  
 }
